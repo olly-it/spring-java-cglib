f8628708fbff57db275e9c0411e5a2c063938620
Alessio Olivieri <alessio.olivieri@enginius.com>
2023-12-22 16:41:05 +0100

    1st commit

---
+++ src/main/java/it/olly/cglib/SpringJavaCglibApplication.java
@@ -0,0 +1,13 @@
+package it.olly.cglib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJavaCglibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJavaCglibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/olly/cglib/SpringJavaCglibApplicationTests.java
@@ -0,0 +1,13 @@
+package it.olly.cglib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJavaCglibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
